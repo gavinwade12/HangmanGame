1fb08a835c45539840cb01ccdfb96c8f701622b0
Gavin <gavin.wade12@gmail.com>
2016-07-14 23:57:36 -0400

    first commit. game about 1/2 done.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+string GetWordToGuess();
+bool CheckUserGuess(string);
+void PrintCurrentString(string[]);
+
+int main() {
+	static int ALLOWED_INCORRECT_GUESSES = 8;
+	int numOfGuesses = 0;
+	bool complete = false;
+	int correctGuesses = 0;
+	char wordToGuess = GetWordToGuess();
+	string userGuess;
+	std::cout << "Welcome to the Hangman Game!" << endl;
+	std::cout << "Getting your word ..." << endl;
+	std::cout << "Got it! Make your first guess: ";
+	std::cin >> userGuess;
+	if (CheckUserGuess(userGuess)) {
+		correctGuesses++;
+		std::cout << "Good guess!" << endl;
+	}
+	else {
+		std::cout << "Sorry, that letter isn't in this word." << endl;
+	}
+
+	numOfGuesses++;
+
+	while (numOfGuesses <= ALLOWED_INCORRECT_GUESSES && !complete) {
+		std::cout << "You've made " << numOfGuesses << " guesses. " << endl;
+		std::cout << "You've made " << numOfGuesses - correctGuesses << " incorrect guesses." << endl;
+		std::cout << ALLOWED_INCORRECT_GUESSES - (numOfGuesses - correctGuesses) << " left until you lose." << endl;
+		std::cout << "Enter your "
+		if (correctGuesses == wordToGuess.length())
+			complete = true;
+	}
+
+}
+
+string GetWordToGuess() {
+
+}
+
+bool CheckUserGuess(string guess) {
+
+}
